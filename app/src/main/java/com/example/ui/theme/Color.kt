fe7430b34737f50b59d9648c194acf508fd95d4d
tiyanzxbellas <tiyabbella@gmail.com>
2026-06-22 17:25:05 +0700

    Add app/src/main/java/com/example/ui/theme/Color.kt

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AmoledBlack = Color(0xFF000000)
+val PureWhite = Color(0xFFFFFFFF)
+val MutedGrey = Color(0xFF8E8E93)
+val BorderGrey = Color(0xFF2C2C2E)
+val CardGrey = Color(0xFF121212)
+val HoverGrey = Color(0xFF1E1E1E)
+val ShimmerGrey = Color(0xFF0A0A0A)
